a64529cd33a3472ba604dd66088a0c7b1e9b2ac5
Trey <trey.delcamp@gmail.com>
2022-08-18 20:48:36 -0600

    commit tree 7c77c0d285c7ebb15818eebcb257ce6b7574aeea

---
+++ index.js
@@ -0,0 +1,18 @@
+// Part 1
+  function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+
+//Part 2 use Two Parameters
+
+function introductionWithLanguage(names, language) {
+    return `Hi, my name is ${names} and I am learning to program in ${language}.`;
+  }
+  
+  //Part 3 Function that uses ann Optional Parameter
+  
+  function introductionWithLanguageOptional(names, language = "JavaScript") {
+    return `Hi, my name is ${names} and I am learning to program in ${language}.`;
+  }
+  
+  
